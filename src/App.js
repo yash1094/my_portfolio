f81da7099788ce9789f58d1d16773765512bab8f
Yash Pandya <yash1094@gmail.com>
2020-12-04 23:07:12 -0500

    Temporarily removed the navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.scss";
 import Body from "./components/Body/Body";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
+// import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
