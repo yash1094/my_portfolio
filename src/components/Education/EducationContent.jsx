701e4c69d73c3349c54442ca24c7d6ae8ed64e6d
Yash Pandya <yash1094@gmail.com>
2020-12-23 15:31:48 -0500

    Added a card element to the education section

--- src/components/Education/EducationContent.jsx
+++ src/components/Education/EducationContent.jsx
@@ -5,33 +5,35 @@ import "./EducationContent.Component.scss";
 const EducationContent = () => {
   return (
     <div className="educationContent">
-      <div className="rows mb-5">
-        <div className="row">
-          <p className="title is-5 mr-3">School :</p>
-          <p className="subtitle">Ontario Tech University, ON</p>
+      <div className="box">
+        <div className="rows mb-5">
+          <div className="row">
+            <p className="title is-5 mr-3">School :</p>
+            <p className="subtitle">Ontario Tech University, ON</p>
+          </div>
+          <div className="row">
+            <p className="title is-5 mr-3">Program :</p>
+            <p className="subtitle">Bachelor of Science, Computer Science</p>
+          </div>
+          <div className="row">
+            <p className="title is-5 mr-3">Graduating :</p>
+            <p className="subtitle">2020</p>
+          </div>
         </div>
-        <div className="row">
-          <p className="title is-5 mr-3">Program :</p>
-          <p className="subtitle">Bachelor of Science, Computer Science</p>
-        </div>
-        <div className="row">
-          <p className="title is-5 mr-3">Graduating :</p>
-          <p className="subtitle">2020</p>
-        </div>
-      </div>
 
-      <div className="rows">
-        <div className="row">
-          <ul className="">
-            Relevent courses:
-            <li className="mt-2">- CSCI 2050U – Computer Architecture</li>
-            <li>- CSCI 2040U – Software Design and Analysis</li>
-            <li>- CSCI 3070U – Analysis and Design of Algorithms</li>
-            <li>- CSCI 3030U – Database Systems and Concepts</li>
-            <li>- CSCI 3060U – Software Quality Assurance</li>
-            <li>- CSCI 3230U - Web Application Development</li>
-            <li>- CSCI 4100U - Mobile Devices</li>
-          </ul>
+        <div className="rows">
+          <div className="row">
+            <ul className="">
+              Relevent courses:
+              <li className="mt-2">- CSCI 2050U – Computer Architecture</li>
+              <li>- CSCI 2040U – Software Design and Analysis</li>
+              <li>- CSCI 3070U – Analysis and Design of Algorithms</li>
+              <li>- CSCI 3030U – Database Systems and Concepts</li>
+              <li>- CSCI 3060U – Software Quality Assurance</li>
+              <li>- CSCI 3230U - Web Application Development</li>
+              <li>- CSCI 4100U - Mobile Devices</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Education/SkillsContent.jsx
+++ src/components/Education/SkillsContent.jsx
@@ -6,63 +6,71 @@ const SkillsContent = () => {
     <div className="skills-content">
       <div className="columns">
         <div className="column">
-          <ul className="larger">
-            Programing Skills:
-            <li>- Languages: Proficient with Python, Java, C++, Dart, PHP</li>
-            <li>
-              <ul>
-                Web Frameworks:
-                <div className="columns">
-                  <div className="column">
-                    <ul>
-                      Front End:
-                      <li>- HTML/CSS/JS/Jquery</li>
-                      <li>- ReactJS</li>
-                      <li>- VueJS</li>
-                      <li>- Svelte</li>
-                    </ul>
+          <div className="box">
+            <ul className="larger">
+              Programing Skills:
+              <li>- Languages: Proficient with Python, Java, C++, Dart, PHP</li>
+              <li>
+                <ul>
+                  Web Frameworks:
+                  <div className="columns">
+                    <div className="column">
+                      <ul>
+                        Front End:
+                        <li>- HTML/CSS/JS/Jquery</li>
+                        <li>- ReactJS</li>
+                        <li>- VueJS</li>
+                        <li>- Svelte</li>
+                      </ul>
+                    </div>
+                    <div className="column">
+                      <ul>
+                        Back End:
+                        <li>- Node/Express</li>
+                        <li>- Flask/Django</li>
+                        <li>- SpringBoot</li>
+                        <li>- Laravel</li>
+                        <li>- Firebase</li>
+                      </ul>
+                    </div>
                   </div>
-                  <div className="column">
-                    <ul>
-                      Back End:
-                      <li>- Node/Express</li>
-                      <li>- Flask/Django</li>
-                      <li>- SpringBoot</li>
-                      <li>- Laravel</li>
-                      <li>- Firebase</li>
-                    </ul>
-                  </div>
-                </div>
-              </ul>
-            </li>
-            <li>- Databases: PostgreSQL, MySQL, MongoDB, FaunaDB, Firestore</li>
-            <li>
-              - Testing, Performance Monitoring, IDE, Frameworks: IntelliJ IDE,
-              Visual Studio JUnit, Maven, ANT, XSLT, Hibernate, Selenium, Jest
-            </li>
-            <li>
-              - Development setup: VScode on MacOS, VM for when windows
-              development is needed
-            </li>
-          </ul>
+                </ul>
+              </li>
+              <li>
+                - Databases: PostgreSQL, MySQL, MongoDB, FaunaDB, Firestore
+              </li>
+              <li>
+                - Testing, Performance Monitoring, IDE, Frameworks: IntelliJ
+                IDE, Visual Studio JUnit, Maven, ANT, XSLT, Hibernate, Selenium,
+                Jest
+              </li>
+              <li>
+                - Development setup: VScode on MacOS, VM for when windows
+                development is needed
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="column">
-          <ul className="larger">
-            Soft Skills
-            <li>- Bilingual: Proficient in English and French</li>
-            <li>
-              - Strong Interpersonal Skills: Able to communicate effectively
-              with clients and colleagues
-            </li>
-            <li>
-              - Time management: Highly organized, consistent and reliable with
-              deadlines
-            </li>
-            <li>- Diligent: Hard working, passionate and responsible</li>
-            <li>
-              - Focused: Make sure tasks are completed to the required standard
-            </li>
-          </ul>
+          <div className="box">
+            <ul className="larger">
+              Soft Skills
+              <li>- Bilingual: Proficient in English and French</li>
+              <li>
+                - Strong Interpersonal Skills: Able to communicate effectively
+                with clients and colleagues
+              </li>
+              <li>
+                - Time management: Highly organized, consistent and reliable
+                with deadlines
+              </li>
+              <li>- Diligent: Hard working, passionate and responsible</li>
+              <li>
+                - Focused: Make sure tasks are completed to the required
+                standard
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
